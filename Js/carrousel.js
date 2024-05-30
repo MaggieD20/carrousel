3139c5f67fa6da148ab8dd693682e9e49ee0d64f
MaggieD20 <coucoumaggie@hotmail.com>
2024-05-30 10:11:07 -0400

    enlever la variable de bouton dans js

--- Js/carrousel.js
+++ Js/carrousel.js
@@ -1,7 +1,6 @@
 (function () {
 
     let carrousel = document.querySelector(".carrousel");
-    let bouton = document.querySelector(".bouton__ouvrir");
     let carrousel__x = document.querySelector(".carrousel__x");
 
     let galerie = document.querySelector(".galerie");
@@ -27,9 +26,6 @@
         AvancerImage();
     });
 
-    bouton.addEventListener("mousedown", function () {
-        carrousel.classList.add("carrousel--ouvrir");
-    });
     carrousel__x.addEventListener("mousedown", function () {
         carrousel.classList.remove("carrousel--ouvrir");
     });
